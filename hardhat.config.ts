328c59d6d11ce5290035c1b35ca61ca30cc8cced
Ido <gershido@gmail.com>
2022-11-07 12:37:09 +0200

    add admin transfer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,6 @@ import 'tsconfig-paths/register';
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-ethers';
-import '@tenderly/hardhat-tenderly';
 import 'hardhat-deploy';
 import 'solidity-coverage';
 import 'hardhat-gas-reporter';
